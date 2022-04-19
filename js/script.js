878de86c357c17fe20f26ced66cdc17c46d369ad
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-04-19 15:06:15 +0000

    fix pronblem

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Madeleine Forgeron All rights reserved
+//
+// Created by: Madeleine Forgeron
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates fahrenfeit to celsius.
+ */
+function calculateCelsius() {
+  // input / Fahrenheit
+  let fahrenheit = parseFloat(document.getElementById('fahrenheit-to-celsius').value)
+  
+  // process calculation
+  let celsius = fahrenheit − 32
+  
+  // output / celsius
+  document.getElementById('celsius-answers').innerHTML = 'The celsius is: ' + celsius.toFixed(0) + ' ° '
+}
